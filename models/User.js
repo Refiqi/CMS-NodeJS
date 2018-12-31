01252df372405ec687dfdd46ff52f03b9292c6fd
refiqi <refyfadhila96@gmail.com>
2018-12-31 12:25:25 +0700

    Feature Register with validation

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+};
+
+const UserSchema = new Schema ({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        validate: [validateEmail, 'Please Fill a Valid Email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        trim: true
+    }
+});
+
+module.exports = mongoose.model('users', UserSchema);
--- routes/home/main.js
+++ routes/home/main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../../models/Post');
 const Category = require('../../models/Category');
+const User = require('../../models/User');
+
 
 router.all('/*', (req, res, next)=> {
     req.app.locals.layout = 'home'; 
@@ -40,6 +42,35 @@ router.get('/about', (req, res)=> {
 router.get('/register', (req, res)=> {
     res.render('home/register');
 });
+
+router.post('/register', (req, res)=>{
+
+    let errors = [];
+
+    const newUser = new User({
+
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    if (req.body.password !== req.body.passwordConfirm) {
+        errors.push({message: "Password field doesn't match"});
+    }
+
+    if(errors.length > 0) {
+        res.render('home/register', {errors: errors});
+    } else {
+        res.send(newUser);
+    }
+
+});
+
+
+
+
+
 
 router.get('/login', (req, res)=> {
     res.render('home/login');
